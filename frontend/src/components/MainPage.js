4c5c98a664a56b17831211151b0db3dd645bd0b7
Marko Alhamadani <Markoalhamadani@gmail.com>
2022-10-26 01:28:19 -0400

    .

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -5,6 +5,12 @@ const MainPage = () => {
     <main className="grid-container">
         <article className="conversation-list">
             This is a test
+            <div className='container'>
+                    <div className="messages">Messages:</div>
+                    <div className="create-a-chat">Create a Chat: </div>
+                    <div className="find-chat">Find a user to chat with</div>
+                    <div className="conversations">Conversations</div>
+            </div>
         </article>
         <article className="conversation-focus">
             This is another test
