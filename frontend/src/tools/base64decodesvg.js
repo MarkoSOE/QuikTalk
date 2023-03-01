d3bf8fba598b1f1cc75edcf1540bf5958c71031c
Marko Alhamadani <Markoalhamadani@gmail.com>
2023-02-28 22:34:26 -0500

    converting image format from base64

--- frontend/src/tools/base64decodesvg.js
+++ frontend/src/tools/base64decodesvg.js
@@ -1,3 +1,13 @@
 import base64 from "react-native-base64";
 
-export default function ConvertToImageFormat(base64ImageFormat, appTitle) {}
+export default function ConvertToImageFormat(base64ImageFormat, appTitle) {
+	let url = base64ImageFormat;
+	if (base64ImageFormat.indexOf("data:image/svg;base64,") > -1) {
+		let decodeSvg = base64.decode(
+			base64ImageFormat.replace("data:image/svg;base64,", "")
+		);
+		let blob = new Blob([decodeSvg], { type: "image/svg+xml" });
+		url = URL.createObjectURL(blob);
+	}
+	return <img src={url} alt={`image for ${appTitle}`} />;
+}
